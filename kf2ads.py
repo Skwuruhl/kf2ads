7beb6362018a66f50779fac153c8090f6c91d141
Skwuruhl <smmarion11@gmail.com>
2024-06-16 06:14:49 -0700

    added support new weapons + scopes

--- kf2ads.py
+++ kf2ads.py
@@ -13,10 +13,15 @@ radialfov = math.radians(fov)
 zooms = [80, 77, 75, 73, 70, 65, 50]
 zooms = [fovMult*z for z in zooms]
 radialzooms = [math.radians(z) for z in zooms]
-normalizations = [normalize(x) for x in range(0,7)]
-keys = ['NumpadOne', 'NumpadTwo', 'NumpadThree', 'NumpadFour', 'NumpadFive', 'NumpadSix', 'NumpadSeven']
+scopes = [48, 27, 20.5]
+radialscopes = [math.radians(s) for s in scopes]
+scopes = [(70/12.5 * 8), (70/23 * 16), (70/18.5 * 12)]
+zooms += scopes
+radialzooms += radialscopes
+normalizations = [normalize(x) for x in range(0,10)]
+keys = ['NumpadZero', 'NumpadOne', 'NumpadTwo', 'NumpadThree', 'NumpadFour', 'NumpadFive', 'NumpadSix', 'NumpadSeven', 'NumpadEight', 'NumpadNine']
 output = ''
-for x in range(0,7):
+for x in range(0,10):
     output += 'SetBind {} "SetZoomedSensitivity {}"|'.format(keys[x], round(normalizations[x],6))
 print('Paste the following into console:', end='\n')
 print(output[:-1])
